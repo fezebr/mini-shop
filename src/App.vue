2c3ff1074275f2b04fa43ab150b235207ef25903
faezeh ebrahimy <fezebr2000@gmail.com>
2025-11-03 09:24:19 +0330

    add ProductFilterBar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="min-h-screen bg-gray-50">
     <NavBar />
+    <ProductFilterBar v-model:category="category" v-model:sort="sort" v-model:search="search" />
     <ProductGrid />
   </div>
 </template>
 
 <script setup lang="ts">
-import ProductGrid from "@/components/ProductGrid.vue";
-import NavBar from "@/components/NavBar.vue";
+  import NavBar from "@/components/NavBar.vue";
+  import ProductGrid from "@/components/ProductGrid.vue";
+  import { ref } from "vue";
+  import ProductFilterBar from "./components/ProductFilterBar.vue";
+
+  const category = ref("");
+  const sort = ref("");
+  const search = ref("");
 </script>
---
+++ src/components/ProductFilterBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="container mx-auto px-6 py-6">
+    <div class="flex gap-4">
+      <XSelectBox 
+        v-model="category" 
+        :options="categoryOptions" 
+        placeholder="Category" 
+        class="w-36"
+      />
+      <XSelectBox 
+        v-model="sort" 
+        :options="sortOptions" 
+        placeholder="Sort by" 
+        class="w-40"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import XSelectBox from "./ui/Xselect.vue";
+
+  const sort = defineModel<string>("sort");
+  const category = defineModel<string>("category");
+
+  const categoryOptions = [
+    { value: "", label: "All" },
+    { value: "watch", label: "Watch" },
+    { value: "phone", label: "Phone" },
+  ];
+
+  const sortOptions = [
+    { value: "", label: "Default" },
+    { value: "price-low", label: "Price ↑" },
+    { value: "price-high", label: "Price ↓" },
+    { value: "name", label: "Name" },
+  ];
+</script>
