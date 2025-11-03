1b6f3d4c9fc971ae76e399fb43d020be723e94ab
faezeh ebrahimy <fezebr2000@gmail.com>
2025-11-03 09:23:57 +0330

    feat: add Font Awesome

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { createApp } from 'vue'
 import { createPinia, setActivePinia } from "pinia";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import './style.css'
-import App from './App.vue'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
 
-//pinia
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas);
+
 const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate)
+pinia.use(piniaPluginPersistedstate);
 setActivePinia(pinia);
 
-
-createApp(App).use(pinia).mount("#app");
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).use(pinia).mount("#app");
